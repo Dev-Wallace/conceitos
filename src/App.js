bf916b7273f761a78719b8997b8048398043e7c5
Dev-Wallace <wallacesoares.bsb@gmail.com>
2022-03-22 22:20:44 -0300

    Primeiro comit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Menu from "./components/Menu";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      
+      <Menu />
+
+      <h1>Hello World</h1>
+    
+      <Button variant="dark">Dark</Button>
+
+    </div>
+);
+}
+
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div>
+
+        <ul>
+            <li><a href=""></a>pagina 1</li>
+            <li><a href=""></a>pagina 2</li>
+            <li><a href=""></a>pagina 3</li>
+        </ul>
+    
+    </div>
+  )
+}
+
+export default Menu
